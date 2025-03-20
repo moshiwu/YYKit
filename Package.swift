7660fdc2b84ff6c7b6c49d7e891566f36fd4f270
莫锹文 <183795044@qq.com>
2025-03-20 14:21:00 +0800

    YYKit support spm

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "YYKit",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "YYKit",
+            targets: ["YYKit"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "libwebp", path: "Vendor/libwebp")
+        
+    ],
+    targets: [
+        .target(
+            name: "YYKit",
+            dependencies: [],
+            path: "YYKit",
+            exclude: [
+                "Base/Foundation/NSObject+YYAddForARC.m",
+                "Base/Foundation/NSThread+YYAdd.m",
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+            ],
+            linkerSettings: [
+                .linkedFramework("UIKit"),
+                .linkedFramework("CoreFoundation"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreText"),
+                .linkedFramework("ImageIO"),
+                .linkedFramework("Accelerate"),
+                .linkedFramework("MobileCoreServices"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("AssetsLibrary"),
+                .linkedFramework("Photos"),
+                .linkedFramework("WebKit"),
+            ]
+        ),
+    ]
+)
